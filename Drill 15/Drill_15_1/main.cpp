c734ecc75f545c2ef2aa915c7745b9d104125dca
Mitró Gyuri <57859881+deckard04@users.noreply.github.com>
2022-03-18 23:22:29 +0100

    Try catch added

--- Drill 15/Drill_15_1/main.cpp
+++ Drill 15/Drill_15_1/main.cpp
@@ -5,58 +5,65 @@
 
 using namespace std;
 
+double one(double x) { return 1; }
+double slope(double x) { return x / 2; }
+double square(double x) { return x * x; }
+double sloping_cos(double x) { return cos(x) + slope(x); }
 
+int main()
+{
+    try
+    {
+        const Point p{300, 300};
+        Simple_window window{Point{100, 100}, 600, 600, "Function Graphs"};
 
-double one(double x) {return 1;}
-double slope(double x) {return x/2;}
-double square(double x) {return x*x;}
-double sloping_cos(double x) {return cos(x)+ slope(x);}
+        Axis x{Axis::x, Point{100, 300}, 400, 20, "1 == 20 pixels"};
+        Axis y{Axis::y, Point{300, 500}, 400, 20, "1 == 20 pixels"};
 
-int main() {
-    const Point p {300, 300};
-    Simple_window window{Point{100, 100}, 600, 600, "Function Graphs"};
+        x.set_color(Color::red);
+        y.set_color(Color::red);
 
-    Axis x {Axis::x, Point{100, 300}, 400, 20, "1 == 20 pixels"};
-    Axis y {Axis::y, Point{300, 500}, 400, 20, "1 == 20 pixels"};
+        window.attach(x);
+        window.attach(y);
 
-    x.set_color(Color::red);
-    y.set_color(Color::red);
+        window.wait_for_button();
 
-    window.attach(x);
-    window.attach(y);
+        const int max_num = 11;
+        const int min_num = -11;
+        const int x_scale = 20;
+        const int y_scale = 20;
+        const int points = 400;
 
-    window.wait_for_button();
+        Function f1{one, min_num, max_num, p, points, x_scale, y_scale};
 
-    const int max_num = 11;
-    const int min_num = -11;
-    const int x_scale = 20;
-    const int y_scale = 20;
-    const int points = 400;
+        Function f2{slope, min_num, max_num, p, points, x_scale, y_scale};
+        Text f2_label{Point{max_num + 50, f2.point(0).y - 10}, "x/2"};
 
-    Function f1 {one, min_num, max_num, p, points, x_scale, y_scale};
+        Function f3{square, min_num, max_num, p, points, x_scale, y_scale};
 
-    Function f2 {slope, min_num, max_num, p, points, x_scale, y_scale};
-    Text f2_label{Point{max_num+50, f2.point(0).y-10}, "x/2"};
+        Function f4{cos, min_num, max_num, p, points, x_scale, y_scale};
+        f4.set_color(Color::blue);
 
-    Function f3 {square, min_num, max_num, p, points, x_scale, y_scale};
+        Function f5{sloping_cos, min_num, max_num, p, points, x_scale, y_scale};
 
-    Function f4 {cos, min_num, max_num, p, points, x_scale, y_scale};
-    f4.set_color(Color::blue);
+        window.attach(f1);
+        window.attach(f2);
+        window.attach(f2_label);
+        window.attach(f3);
+        window.attach(f4);
+        window.attach(f5);
 
-    Function f5 {sloping_cos, min_num, max_num, p, points, x_scale, y_scale};
-
-
-
-    window.attach(f1);
-    window.attach(f2);
-    window.attach(f2_label);
-    window.attach(f3);
-    window.attach(f4);
-    window.attach(f5);
-    
-    window.wait_for_button();
-
-
-
+        window.wait_for_button();
+    }
+    catch (exception &e)
+    {
+        cerr << "exception: " << e.what() << endl;
+        keep_window_open();
+    }
+    catch (...)
+    {
+        cerr << "exception\n";
+        keep_window_open();
+    }
     return 0;
 }
